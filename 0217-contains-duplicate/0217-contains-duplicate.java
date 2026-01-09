57059fd8e9e5dee01472c5a8b2c03e5a5de3ae7e
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-01-09 21:17:11 +0530

    Time: 10 ms (99.2%), Space: 97.8 MB (10.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int found=0;
-        for(int i=1;i<nums.length;i++) {
-            if(nums[i]==nums[i-1]){
-                found=1;
-                break;
-            }
+        HashSet<Integer> seen=new HashSet<>();
+        for(int x:nums){
+            if(!seen.add(x))    return true;
         }
-        if(found == 1) return true;
-        else    return false;
+        return false;
     }
 }
