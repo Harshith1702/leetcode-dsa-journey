9df7c7bdb2fe607be460114fab8539bfc16c9c52
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-01-10 18:06:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int n=s1.length(),m=s2.length();
+        int[] dp=new int[n+1];
+        for(int i=n-1;i>=0;i--){
+            int prev=0;
+            for(int j=m-1;j>=0;j--){
+                int temp=dp[j];
+                if(s1.charAt(i)==s2.charAt(j))
+                    dp[j]=s1.charAt(i)+prev;
+                else
+                    dp[j]=Math.max(dp[j],dp[j+1]);
+                prev=temp;
+            }
+        }
+        int sum=0;
+        for(char c:s1.toCharArray())    sum+=c;
+        for(char c:s2.toCharArray())    sum+=c;
+
+        return sum-2*dp[0];
+    }
+}
