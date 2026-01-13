6d4537c3c6b59be6d2ab906d8f95519c3ee68778
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-01-13 18:45:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double separateSquares(int[][] squares) {
+        double totalArea=0,low=Double.maxMAX_VALUE,high=0;
+        for(int[] s:squares){
+            double y=s[1],l=s[2];
+            totalArea+=l*l;
+            low=Math.min(low,y);
+            high=Math.max(high,y+l);
+        }
+        double target=totalArea/2.0;
+        while(high-low > 1e-5){
+            double mid=(low+high)/2;
+            double below=0;
+            for(int[] s:squares){
+            double y=s[1],l=s[2];
+        }
+    }
+}
