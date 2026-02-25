df4f812b7d67463364e0ffa03e266f9831c21aa6
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-02-25 19:08:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -2,13 +2,11 @@ class Solution {
     public int repeatedStringMatch(String a, String b) {
         StringBuilder s=new StringBuilder(a);
         int c=1;
-        while(s.length()<b.length()){
+        while(s.length()<b.length()+a.length()){
             s.append(a);
             c++;
+            if(s.indexOf(b)!=-1)   return c;
         }
-        if(s.indexOf(b)!=-1)   return c;
-        s.append(a);
-        if(s.indexOf(b)!=-1)   return c+1;
         return -1;
     }
 }
