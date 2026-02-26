ef808b0fee915b191d4e84eb600467cdbee5f7ed
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-02-26 19:16:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (96.86%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int c=0;
+        while(target>0 && maxDoubles>0){
+            if(target%2==0){
+                target/=2;
+                maxDoubles--;
+            }
+            else target--;
+            c++;
+        }
+        return c+(target-1);
+    }
+}
