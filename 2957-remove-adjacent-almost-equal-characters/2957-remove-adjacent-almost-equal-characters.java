a0f6fd170d83571a50146434732ab90920528e2c
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-03-05 18:58:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.java
+++ 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.java
@@ -1,9 +1,9 @@
 class Solution {
     public int removeAlmostEqualCharacters(String word) {
         int c=0;
-        for(int i=0;i<word.length()-1;i++){
-            int condition=Math.abs(word.charAt(i)-word.charAt(i+1));
-            if(condition<=1){
+        int n=word.length();
+        for(int i=0;i<n-1;i++){
+            if(Math.abs(word.charAt(i)-word.charAt(i+1))<=1){
                 c++;
                 i++;
             }
