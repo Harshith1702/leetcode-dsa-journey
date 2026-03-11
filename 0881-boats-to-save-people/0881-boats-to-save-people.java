a62fa35f37ed50fc87be7e58bec48eede229dcbe
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-03-11 21:32:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -4,11 +4,12 @@ class Solution {
         int n=people.length;
         int i=0,j=n-1;
         int b=0;//boats
-        while(i<=j){
+        while(i<j){
             if(people[i]+people[j]<=limit)  i++;
             j--;
             b++;
         }
+        if(i==j)    b++;
         return b;
     }
 }
