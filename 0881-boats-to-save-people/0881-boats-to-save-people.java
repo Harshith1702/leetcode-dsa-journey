2af6ed7f90e0fb43644cc057035296ac5c2dd82c
PADAKANTI HARSHITH <harshaharshith31@gmail.com>
2026-03-11 21:30:30 +0530

    Time: 21 ms (23.07%), Space: 56.4 MB (87.93%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int n=people.length;
+        int i=0,j=n-1;
+        int b=0;//boats
+        while(i<=j){
+            if(people[i]+people[j]<=limit)  i++;
+            j--;
+            b++;
+        }
+        return b;
+    }
+}
