59830c70b24b52d634b686f388ce3c88249f26c8
Harshith Padakanti <harshaharshith31@gmail.com>
2026-09-09 22:46:22 +0530

    Time: 1 ms (99.36%), Space: 43 MB (11.54%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,10 +1,10 @@
 class Solution {
     public long countCommas(long n) {
-        long c=0;
-        long m=1000;
-        while(m<=n){
-            c+=(n-m+1);
-            m*=1000;
+        long c = 0;
+        long m = 1000;
+        while (m <= n){
+            c += (n-m+1);
+            m *= 1000;
         }
         return c;
     }
